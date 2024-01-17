c5e46837147340130b4fed7d1e81a13651ee21f2
AYAABDELNABI <aya.abdelnabi.sw@gmail.com>
2024-01-17 09:05:41 +0200

    AA

--- javascript-warm_up/103-object_fct.js
+++ javascript-warm_up/103-object_fct.js
@@ -1,17 +1,22 @@
-
 const myObject = {
-    type: 'object',
-    value: 12,
-    incr: function() {
-      this.value++;
-    }
-  };
-  console.log(myObject);
-  
-  myObject.incr();
-  console.log(myObject);
-  myObject.incr();
-  console.log(myObject);
-  myObject.incr();
-  console.log(myObject);
+  type: 'object',
+  value: 12,
+  incr: function() {
+    this.value++;
+  }
+};
+
+console.log(myObject);
+
+myObject.incr();
+console.log(myObject);
+
+myObject.incr();
+console.log(myObject);
+
+myObject.incr();
+console.log(myObject);
+
+
+
   
